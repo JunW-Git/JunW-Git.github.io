ffa4c1fc09e2074829cdf826446b4e5ab69f9ac1
JunW-Git <jia.jun.eight@gmail.com>
2025-03-04 10:06:09 +1100

    fixed js

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ function dropbtn() {
     document.getElementById("dropbtn").classList.toggle("show");
     console.log("Button is being clicked!!!");
 }
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbutton')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i]
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
